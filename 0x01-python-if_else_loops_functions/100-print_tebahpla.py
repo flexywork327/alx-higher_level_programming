bccc57f36a5fb4a6cfccbe529bedc1d1c542503f
Felix Asare <flexywork327@gmail.com>
2023-01-05 03:55:14 +0000

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
-    print('{}'.format(chr(i - diff)), end='')
+i = 122
+while i >= 97:
+    flag = 0
+    if i % 2 != 0:
+        i = i - 32
+        flag = 1
+    print("{:s}".format(chr(i)), end="")
+    if flag == 1:
+        i = i + 32
+    i = i - 1
+    
