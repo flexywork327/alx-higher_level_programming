31b73d8f7433dc88fb1a3c9cddb43dab39a9209d
Felix Asare <flexywork327@gmail.com>
2023-04-12 16:56:08 +0000

    103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,13 +4,17 @@ const myObject = {
   type: 'object',
   value: 12
 };
+
 console.log(myObject);
 myObject.incr = () => {
   myObject.value++;
 };
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
