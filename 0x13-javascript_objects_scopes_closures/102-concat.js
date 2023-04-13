a3bdc8700a02c37b425debd666a386d296272ae0
Felix Asare <flexywork327@gmail.com>
2023-04-13 14:36:05 +0000

    [fix]:102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,34 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+// Adding the ffilesystem Moule
+const fSys = require('fs');
 
-// Read the command-line arguments
-const sourceFile1 = process.argv[2];
-const sourceFile2 = process.argv[3];
-const destinationFile = process.argv[4];
-
-// Open the first source file and read its contents
-fs.readFile(sourceFile1, 'utf8', function (err, data1) {
-  if (err) {
-    return console.log(err);
-  }
-
-  // Open the second source file and read its contents
-  fs.readFile(sourceFile2, 'utf8', function (err, data2) {
-    if (err) {
-      return console.log(err);
-    }
-
-    // Concatenate the contents of the two source files
-    const concatenatedData = data1 + data2;
-
-    // Write the concatenated data to the destination file
-    fs.writeFile(destinationFile, concatenatedData, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-
-      console.log(`Successfully concatenated ${sourceFile1} and ${sourceFile2} into ${destinationFile}.`);
-    });
-  });
-});
+const fileA = fSys.readFileSync(process.argv[2]).toString();
+const fileB = fSys.readFileSync(process.argv[3]).toString();
+fSys.writeFileSync(process.argv[4], fileA + fileB);
